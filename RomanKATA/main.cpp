38549569510979a5e1c910958a0dcbddd799d288
aqwalters1212 <Adam@Adams-MacBook-Pro.local>
2015-02-20 12:19:59 -0900

    Added test 1 for if function works

---
+++ RomanKATA/main.cpp
@@ -0,0 +1,28 @@
+
+//Adam Walters
+//CS372
+//Roman Numeral KATA
+//Program that handles Roman Numerals and uses TDD.
+//Based on algorithm found at http://www.slideshare.net/JonJagger/larry-and-jen-do-roman-numerals-in-c
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "Catch.hpp"
+#include <string>
+#include <sstream>
+
+
+using std::string;
+using std::cout;
+using std::endl;
+
+string Converter(int number)
+{
+    return "";
+}
+
+TEST_CASE( "Converter Function", "Convert" ) {
+    REQUIRE( Converter(4) == "");
+    
+    
+}
